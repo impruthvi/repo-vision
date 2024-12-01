680287507229ac9c00ac2898c72d9ea112b210ad
impruthvi <pruthvirajput97@gmail.com>
2024-12-01 11:15:54 +0530

    Add authentication layout and pages with Clerk integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default async function Home() {
-  return <h1 className="text-red-500">Hello</h1>;
+  return (
+    <h1 className="text-red-500">
+      <UserButton />
+    </h1>
+  );
 }
