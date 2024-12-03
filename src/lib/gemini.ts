5d0ef3c1b1a63edb4802240e05bf9901e617a6d3
impruthvi <pruthvirajput97@gmail.com>
2024-12-03 21:20:56 +0530

    Add aiSummarizeCode function for code snippet explanations and generateEmbedding function for summary embeddings

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,4 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { Document } from "@langchain/core/documents";
 
 const gemini = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
@@ -47,8 +48,32 @@ ${diff}`;
   try {
     const response = await model.generateContent([promptTemplate]);
     return response.response.text();
-} catch (error) {
+  } catch (error) {
     console.error("Error summarizing the commit diff:", error);
     throw new Error("Failed to generate a commit summary.");
   }
+};
+
+export const aiSummarizeCode = async (doc: Document) => {
+  const code = doc.pageContent.slice(0, 1000); // Limit to 1000 characters
+  const response = await model.generateContent([
+    `You are an experienced senior software engineer specializing in onboarding junior engineers to new codebases.`,
+    `Your task is to explain the purpose and functionality of the following code snippet from the file "${doc.metadata.source}" to a junior engineer in simple terms.`,
+    `Here is the code snippet:
+    ---
+    ${code}
+    ---
+    Provide a concise summary (no more than 100 words) explaining what the code does and its purpose in the context of the codebase. Focus on clarity and simplicity.`,
+  ]);
+
+  return response.response.text();
+};
+
+export const generateEmbedding = async (summery: string) => {
+  const model = gemini.getGenerativeModel({
+    model: "text-embedding-004",
+  });
+  const result = await model.embedContent(summery);
+  const embedding = result.embedding;
+  return embedding.values;
 };
